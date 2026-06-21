b61d852430ceff9b5d20ec55039f451c88dcb579
Wirenux <louis.lesniak2@gmail.com>
2026-06-21 14:58:57 +0200

    fix: add missing attributes to login form elements and improve form handling

--- src/components/LoginScreen.js
+++ src/components/LoginScreen.js
@@ -9,11 +9,12 @@ export function renderLoginScreen(parent) {
             <div class="title-bar">
                 <div class="title-bar-text">Welcome to Windows</div>
                 <div class="title-bar-controls">
-                    <button aria-label="Help"></button>
-                    <button aria-label="Close"></button>
+                    <button aria-label="Help" type="button"></button>
+                    <button aria-label="Close" type="button"></button>
                 </div>
             </div>
-            <div class="window-body login-grid">
+
+            <form id="login-form" class="window-body login-grid">
                 <div class="login-main">
 
                     <div class="login-instruction">
@@ -23,19 +24,32 @@ export function renderLoginScreen(parent) {
 
                     <div class="field-row login-field">
                         <label for="username"><span class="underline">U</span>ser name:</label>
-                        <input id="username" type="text" value="kinger"/>
+                        <input id="username" name="username" type="text" value="kinger"/>
                     </div>
                     <div class="field-row login-field">
                         <label for="password"><span class="underline">P</span>assword:</label>
-                        <input id="password" type="password"/>
+                        <input id="password" name="password" type="password"/>
                     </div>
                 </div>
 
                 <div class="login-actions">
                     <button id="login-submit" type="submit">OK</button>
-                    <button id="login-cancel">Cancel</button>
+                    <button id="login-cancel" type="button">Cancel</button>
                 </div>
-            </div>
+            </form>
         </div>
     `
+
+    const loginForm = document.getElementById('login-form');
+
+    loginForm.addEventListener('submit', (e) => {
+        e.preventDefault();
+
+        const username = loginForm.username.value;
+        const password = loginForm.password.value;
+
+        if (username === 'kinger' && password === "quennie123") {
+            console.log("Log in");
+        }
+    })
 }
