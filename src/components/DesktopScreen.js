f73de5ba4566b33a98a875791698954430d7718a
Wirenux <louis.lesniak2@gmail.com>
2026-06-23 20:29:03 +0200

    add: implement desktop taskbar with clock and start menu button + icon

--- src/components/DesktopScreen.js
+++ src/components/DesktopScreen.js
@@ -1,5 +1,22 @@
 import '../style.css'
+import '../desktop.css'
+
 import { changeState } from '../main.js'
+
+let clockInterval;
+
+function updateClock() {
+  const clock = document.querySelector('.clock');
+  if (!clock) return;
+
+  const time = new Date().toLocaleString('en-US', {
+    hour: 'numeric',
+    minute: 'numeric',
+    hour12: true
+  });
+
+  clock.textContent = time;
+}
 
 export function renderDesktop(parent) {
     parent.innerHTML = `
@@ -7,13 +24,31 @@ export function renderDesktop(parent) {
           <div id="workspace"></div>
 
           <div id="taskbar">
+            <button class="start-menu-btn">
+              <img class="start-btn-image" src="/icons/logo.png" />
+              <span>Start</span>
+            </button>
+
+            <div id="start-menu">
+              <div class="start-menu-item">Test1</div>
+              <div class="start-menu-item">Test2</div>
+              <div class="start-menu-item">Test3</div>
+              <div class="start-menu-item">Test4</div>
+            </div>
+
             <div id="taskbar-apps">
             </div>
 
-            <div id="system-tray">
-              <div class="clock">09:04 AM</div>
+            <div id="clock-widget" class="field-border-disabled" style="padding: 7px 8px 5px">
+              <img class="sound-img-clock" src="/icons/sound.png" />
+              <div class="clock">--:-- xM</div>
             </div>
           </div>
         </div>
     `
+
+    updateClock();
+
+    clearInterval(clockInterval);
+    clockInterval = setInterval(updateClock, 1000);
 }
